17eacab2b03b86d8b97a20086e62d37b37f14510
MaksimZ91 <ak4202@mail.ru>
2020-12-29 10:43:10 +0800

    complete

---
+++ index.js
@@ -0,0 +1,14 @@
+const qustionList =document.querySelectorAll('.question_quest')
+
+qustionList.forEach((element, index)=>{
+  element.addEventListener('click',function (){
+    this.classList.toggle("active");
+  let content = this.nextElementSibling;
+  if (content.style.display === "block") {
+    content.style.display = "none";
+  } else {
+    content.style.display = "block";}
+  })
+})
+
+
